8cddc567133270a2919fc04e325acc44229c3697
Naman1302 <76490008+Naman1302@users.noreply.github.com>
2021-05-29 16:29:54 +0530

    Update 21. MST@Prim's Algorithm.cpp

--- Graph/21. MST@Prim's Algorithm.cpp
+++ Graph/21. MST@Prim's Algorithm.cpp
@@ -1,3 +1,4 @@
+//Adjacency List + Priority Queue
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -50,3 +51,44 @@ int main()
     cout << minimumCost << endl;
     return 0;
 }
+
+// Adjacency Matrix+arrays(GFG)
+int min_ver(vector<int> &value,vector<bool> &mst){
+        int min=INT_MAX,index;
+        for(int i=0;i<value.size();i++){
+            if(mst[i]==false and value[i]<min){
+                min=value[i];
+                index=i;
+            }
+        }
+        return index;
+    }
+    int spanningTree(int v, vector<vector<int>> adj[])
+    {
+        // code here
+        int parent[v];
+        vector<bool> mst(v,false);
+        vector<int> value(v,INT_MAX);
+        
+        parent[0]=-1;
+        value[0]=0;
+        
+        int sum=0;
+        
+        for(int i=0;i<v-1;i++){
+            int u=min_ver(value,mst);
+            mst[u]=true;
+            for(auto j: adj[u]){
+                vector<int> V=j;
+                int w=V[1],node=V[0];
+                if(w and value[node]>w && mst[node]==false){
+                    value[node]=w;
+                    parent[node]=u;
+                }
+            }
+        }
+        for(int i=1;i<v;i++){
+            sum+=value[i];
+        }
+        return sum;
+    }
