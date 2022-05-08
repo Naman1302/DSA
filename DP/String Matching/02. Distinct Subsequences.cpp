9d5fb1b1b12cc6a5dd6d1e9b027ed7eab9f69307
Naman1302 <76490008+Naman1302@users.noreply.github.com>
2022-05-08 20:50:19 +0530

    1-D Optimisation

--- DP/String Matching/02. Distinct Subsequences.cpp
+++ DP/String Matching/02. Distinct Subsequences.cpp
@@ -32,3 +32,33 @@ public:
         //return solve(s,t,s.length()-1,t.length()-1,dp);
     }
 };
+//1-D Optimisation
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        //vector<vector<int>> dp(s1.length()+1,vector<int>(s2.length()+1,0));
+        
+        int n=s1.length(),m=s2.length();
+        
+        vector<int> prev(m+1,0),curr(m+1,0);
+        
+        /*for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }*/
+        
+        for(int j=0;j<=m;j++){
+            prev[j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            curr[0]=i;
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1])
+                    curr[j]=prev[j-1];
+                else
+                    curr[j]=min(1+prev[j-1],min(1+prev[j],1+curr[j-1]));
+            }
+            prev=curr;
+        }
+        return prev[m];
+    }
+};
